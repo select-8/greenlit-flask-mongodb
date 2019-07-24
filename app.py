7a0a81e1d7dc5e36388941615f3b27f43d455509
Liam O'Connor <liamoconnor8481@gmail.com>
2019-07-24 12:58:02 +0100

    added some css. added route to show genres in dropdown menu

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET", "randomstring123")
 
 app.config["MONGO_DBNAME"] = 'greenlit'
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
@@ -18,13 +19,25 @@ def add_user():
     user_collection.insert({"username":"Johnny Johns"})
     return redirect(url_for('show_user'))
 
-@app.route('/show_user')
 """
 should show session user and list of pitches with option to edit
 and maybe to add a profile?
 """
+@app.route('/show_user')
 def show_user():
     return render_template("show_users.html", users=mongo.db.users.find())
+
+@app.route('/add_pitch')
+def add_pitch():
+    _genres = mongo.db.genres.find()
+    genre_list = [genre for genre in _genres]
+    # _directors = mongo.db.directors.find()
+    # director_list = [direct for directors in _directors]
+    # _actors = mongo.db.talent.find()
+    # actor_list = [act for actors in _actors]
+    # return render_template('add_pitch.html', genres = genre_list, directors = director_list, actors=actor_list)
+    return render_template('add_pitch.html', genres = genre_list)
+
 
 @app.route('/show_pitches')
 def show_pitches():
@@ -32,7 +45,7 @@ def show_pitches():
 
 
 @app.route('/add_pitch/<username>/<title>/<desc>/<director>/<actor>')
-def add_pitch(username, title, desc, director, actor):
+def insert_pitch(username, title, desc, director, actor):
     user = mongo.db.users
     the_user = user.find_one({'username' : username})
 
