6407f178e709a6f795ced2e1857746e40c5c2129
Liam O'Connor <liamoconnor8481@gmail.com>
2019-07-24 19:36:26 +0100

    user can register and/or login. Session username is inserted to pitches when pitch is inserted. All pitches can be viewed by user

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+import bcrypt
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET", "randomstring123")
@@ -10,6 +11,41 @@ app.config["MONGO_DBNAME"] = 'greenlit'
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    # if 'username' in session:
+    #     return render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    error = None
+    users = mongo.db.users
+    login_user = users.find_one({'username' : request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
+            session['username'] = request.form['username']
+            return redirect(url_for('add_pitch'))
+        return 'Invalid Password'
+        # return render_template('login.html', error = error)
+    # return 'Invalid Username'
+    return render_template('index.html', error = error)
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'username' : request.form['username']})
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'username' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        return 'That username already exists!'
+    return render_template('register.html')
 
 @app.route('/add_user')
 def add_user():
@@ -37,11 +73,20 @@ def add_pitch():
     actor_list = [actors for actors in _actors]
     return render_template('add_pitch.html', genres = genre_list, directors = director_list, actors=actor_list)
 
+
 @app.route('/user_pitch', methods=['POST'])
 def user_pitch():
     pitch = mongo.db.pitches
-    pitch.insert_one(request.form.to_dict())
+    title=request.form.get('title')
+    genre_name=request.form.get('genre_name')
+    director_name=request.form.get('director_name')
+    actor_name=request.form.get('actor')
+    description=request.form.get('discription')
+    username=session['username']
+    # pitch.insert_one(request.form.to_dict())
+    pitch.insert_one({'username': username, 'title': title, 'genre_name':genre_name, 'director_name':director_name, 'actor': actor_name, 'description': description})
     return redirect(url_for('add_pitch'))
+
 
 @app.route('/add_pitch/<username>/<title>/<desc>/<director>/<actor>')
 def insert_pitch(username, title, desc, director, actor):
