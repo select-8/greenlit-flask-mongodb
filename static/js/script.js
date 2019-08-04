52bf5bb4fcaafda1b23fa5fcd2dd63c74cf33c88
Liam O'Connor <liamoconnor8481@gmail.com>
2019-08-04 22:59:29 +0100

    added film reel styles to pitches

--- static/js/script.js
+++ static/js/script.js
@@ -1,33 +1,8 @@
-var titles = document.getElementsByClassName('title')
-for (var i = 0; i < titles.length; i++) {
-    console.log(titles[i].innerHTML)
-}
-
-
-$('#rating_sort').on("click", function () {
-    reorderMyData();
+$(document).ready(function () {
+    //variable should be cached for performance
+    const p = $("#content");
+    p.hide();
+    $("#drop-down").click(function () {
+        p.slideToggle("slow");
+    });
 });
-
-var reorderMyData = function () {
-    var order = get_ratings_and_sort
-    // https://stackoverflow.com/questions/929519/dynamically-arranging-divs-using-jquery
-    var div = $("#make-reorderable-by-rating");
-    var children = div.children();
-    div.empty();
-    for (var i = 0; i < order.length; i++) {
-        div.append(children[order[i]])
-    }
-}
-
-var div = $("#make-reorderable-by-rating");
-var children = div.children();
-console.log(children)
-
-
-// http://jsfiddle.net/bittu4u4ever/ezYJh/1/
-var divList = $(".listing-item");
-divList.sort(function (a, b) {
-    return $(a).data("listing-price") - $(b).data("listing-price")
-});
-
-$("#list").html(divList);
